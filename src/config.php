e44c530cc6f6fb1bddea8f417a9d14233eb6f623
root <root@v597564457.local>
2026-01-29 09:28:22 +0300

    Удален config.php из репозитория, файл оставлен локально

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-  'smtp_host'   => 'smtp.yandex.ru',
-  'smtp_user'   => 'youremail',
-  'smtp_pass'   => 'yourpass',
-  'smtp_port'   => 465,             
-  'from_email'  => 'youremail',
-  'from_name'   => 'MySite',
-];
